9551abd79b9250cc6ef974b1cbb0afbbad91eb36
Soohyun Park <pshyun03@gmail.com>
2025-03-17 14:54:08 +0900

    feat(#3) : user,medicine,storage 도메인 구현

---
+++ src/main/java/com/Mefo/server/domain/userInfo/entity/UserDrink.java
@@ -0,0 +1,12 @@
+package com.Mefo.server.domain.userInfo.entity;
+
+public enum UserDrink {
+    RARELY("0~1회"),
+    SOMETIMES("2~3회"),
+    OFTEN("4~5회"),
+    ALWAYS("6~7회");
+
+    private String times;
+
+    UserDrink(String times){this.times=times;}
+}
---
+++ src/main/java/com/Mefo/server/domain/userInfo/entity/UserSex.java
@@ -0,0 +1,5 @@
+package com.Mefo.server.domain.userInfo.entity;
+
+public enum UserSex {
+    MALE, FEMALE
+}
